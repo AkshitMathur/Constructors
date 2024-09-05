cc6d9c0e72428e0b0ca543c85ed0deaf9ec1b214
AkshitMathur <mathur.aksh2712@gmail.com>
2024-09-05 20:55:55 +0530

    Uploaded Parameterized Constructor

---
+++ Parameterized_Constructors.cpp
@@ -0,0 +1,29 @@
+//23070123014 Akshit Mathur
+//Aim:Implemention of Constructors to perform using Parameterized Constructors.
+#include <iostream>
+using namespace std;
+
+class Construct {
+    int a;
+    int b;
+public:
+    Construct(int m , int n) {
+        a = m;
+        b = n;
+    }
+    void display() {
+        cout << "a ="<< a <<endl<< "b ="<<b;
+    }
+};
+
+int main() {
+    Construct myconstruct(10,20);
+    myconstruct.display();
+
+    return 0;
+}
+//Output
+/*a = 10
+b = 20
+*/
+
