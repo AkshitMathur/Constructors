7692235546e301de3885c1609c48a15738564f44
AkshitMathur <mathur.aksh2712@gmail.com>
2024-09-05 21:07:33 +0530

    Uploaded Constructors_Outside_Classes.

---
+++ Constructors_Outside_Classes.cpp
@@ -0,0 +1,38 @@
+//23070123014 Akshit Mathur
+//Aim:Implemention of Constructors to Define Constructor Outside Class
+#include <iostream>
+using namespace std;
+
+class Student {
+    int rno;
+    char name[50];
+    double fee;
+public:
+    Student();
+    void display();
+};
+
+Student::Student() {
+    cout << "Enter the roll number: ";
+    cin >> rno;
+    cout << "Enter the name: ";
+    cin >> name;
+    cout << "Enter the fee: ";
+    cin >> fee;
+}
+
+void Student::display() {
+    cout << endl << rno << "\t" << name << "\t" << fee << endl;
+}
+
+int main() {
+    Student s;
+    s.display();
+    return 0;
+}
+//Output
+/*Enter the roll number: 014
+Enter the name: Akshit
+Enter the fee: 1000000
+
+14      Akshit  1e+006*/
